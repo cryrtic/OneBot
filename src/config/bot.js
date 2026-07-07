3e39d6d7ec73955fc036ac6b158cb7a14034a997
cryrtic <finnavte@icloud.com>
2026-07-07 15:16:32 +0100

    Update bot activity message

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "made by cryrtic. if you want me send cryrtic. as friend or send me an dm!",
         // Activity type number (0 = Playing).
         type: 0,
       },
